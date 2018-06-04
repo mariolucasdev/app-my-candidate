44a878664ad5ed222343800472bf452a18d9ffff
Henrique Nascimento <henriquenascandrade@gmail.com>
2018-06-04 17:49:01 -0300

    update component Info

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 
 export default () => (
-  <View>
-    <Text />
+  <View style={styles.content}>
+    {/* <Image source={require("./imgs/logo.png")} /> */}
+    <Text style={styles.text}>twitter: @he_nasc</Text>
+    <Text style={styles.text}>https://github.com/henriquenasc</Text>
+    <Text style={{ color: "#fff", fontWeight: "bold" }}>
+      -------------------------------------------------------
+    </Text>
+    <Text style={styles.text}>versão: 1.0.0</Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2980b9"
+  },
+
+  text: {
+    margin: 5,
+    padding: 10,
+    backgroundColor: "#fff",
+    color: "#2980b9",
+    fontSize: 18,
+    fontWeight: "bold",
+    borderRadius: 10
+  }
+});
